a3173ba7f964dd75c0cb07ff5f7804c8cc58b7e9
ozan.dundar <ozan.dundar@crosstech.com.tr>
2020-02-25 12:54:45 +0300

    first push

---
+++ script.js
@@ -0,0 +1,251 @@
+var data = [
+  {
+    'Zaman damgası': '2/24/2020 18:20:16',
+    'Adınız Soyadınız': 'Ozan Dundar1',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [1.tercihiniz]': 'Eğitim',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [2.tercihiniz]': 'Kariyer',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [3.tercihiniz]': 'Kişisel Gelişim ve Sanat',
+    'Eğitim kategorisinde hangi atölyeleri seçmek istersiniz?': 'SQL, Javascript',
+    'Kariyer kategorisinde hangi atölyeleri seçmek istersiniz?': 'Kariyer Yolu Belirleme',
+    'Kişisel Gelişim ve Sanat kategorisinde hangi atölyeleri seçmek istersiniz?': 'Hikayeleştime',
+    'Telefon Numaranız': '5397062143',
+    'Hangi okuldasınız?': 'Beykent Universitesi',
+    ' Bölümünüz-Sınıfınız ': 'Bilgisayar Muhendisligi - 4',
+    'Egitim kategorisinde hangi egitimleri secmek istersiniz?': 'SQL, Java',
+    'Kariyer kategorisinde hangi egitimleri secmek istersiniz?':
+      'Mülakat Teknik Ve Simülasyonları LERZAN ERKAN ( Saat 14:00-17:30)',
+    'Kişisel Gelişim kategorisinde hangi egitimleri secmek istersiniz?':
+      'Hikayeleştirme-STORYTELLING (Saat 13:00-14:30)',
+    'Sanat kategorisinde hangi egitimleri secmek istersiniz?': 'C EĞİTİMİ - SAAT BİLGİSİ',
+    'Mail Adresiniz': 'ozn.dndr921@gmail.com',
+    'Öğrenci misiniz?': 'Evet'
+  },
+  {
+    'Zaman damgası': '2/24/2020 18:20:16',
+    'Adınız Soyadınız': 'Ozan Dundar2',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [1.tercihiniz]': 'Eğitim',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [2.tercihiniz]': 'Kariyer',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [3.tercihiniz]': 'Kişisel Gelişim ve Sanat',
+    'Eğitim kategorisinde hangi atölyeleri seçmek istersiniz?': 'SQL, Java',
+    'Kariyer kategorisinde hangi atölyeleri seçmek istersiniz?': 'Kariyer Yolu Belirleme',
+    'Kişisel Gelişim ve Sanat kategorisinde hangi atölyeleri seçmek istersiniz?': 'Hikayeleştime',
+    'Telefon Numaranız': '5397062143',
+    'Hangi okuldasınız?': 'Beykent Universitesi',
+    ' Bölümünüz-Sınıfınız ': 'Bilgisayar Muhendisligi - 4',
+    'Egitim kategorisinde hangi egitimleri secmek istersiniz?': 'SQL-Cengiz Atilla (Saat:12:00-17:30 )(2 gün)',
+    'Kariyer kategorisinde hangi egitimleri secmek istersiniz?':
+      'Mülakat Teknik Ve Simülasyonları LERZAN ERKAN ( Saat 14:00-17:30)',
+    'Kişisel Gelişim kategorisinde hangi egitimleri secmek istersiniz?':
+      'Hikayeleştirme-STORYTELLING (Saat 13:00-14:30)',
+    'Sanat kategorisinde hangi egitimleri secmek istersiniz?': 'C EĞİTİMİ - SAAT BİLGİSİ',
+    'Mail Adresiniz': 'ozn.dndr921@gmail.com',
+    'Öğrenci misiniz?': 'Evet'
+  },
+  {
+    'Zaman damgası': '2/24/2020 18:20:16',
+    'Adınız Soyadınız': 'Ozan Dundar3',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [1.tercihiniz]': 'Eğitim',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [2.tercihiniz]': 'Kariyer',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [3.tercihiniz]': 'Kişisel Gelişim ve Sanat',
+    'Eğitim kategorisinde hangi atölyeleri seçmek istersiniz?': 'SQL, Javascript, Java',
+    'Kariyer kategorisinde hangi atölyeleri seçmek istersiniz?': 'Kariyer Yolu Belirleme',
+    'Kişisel Gelişim ve Sanat kategorisinde hangi atölyeleri seçmek istersiniz?': 'Hikayeleştime',
+    'Telefon Numaranız': '5397062143',
+    'Hangi okuldasınız?': 'Beykent Universitesi',
+    ' Bölümünüz-Sınıfınız ': 'Bilgisayar Muhendisligi - 4',
+    'Egitim kategorisinde hangi egitimleri secmek istersiniz?': 'SQL-Cengiz Atilla (Saat:12:00-17:30 )(2 gün)',
+    'Kariyer kategorisinde hangi egitimleri secmek istersiniz?':
+      'Mülakat Teknik Ve Simülasyonları LERZAN ERKAN ( Saat 14:00-17:30)',
+    'Kişisel Gelişim kategorisinde hangi egitimleri secmek istersiniz?':
+      'Hikayeleştirme-STORYTELLING (Saat 13:00-14:30)',
+    'Sanat kategorisinde hangi egitimleri secmek istersiniz?': 'C EĞİTİMİ - SAAT BİLGİSİ',
+    'Mail Adresiniz': 'ozn.dndr921@gmail.com',
+    'Öğrenci misiniz?': 'Evet'
+  },
+  {
+    'Zaman damgası': '2/24/2020 18:20:16',
+    'Adınız Soyadınız': 'Ozan Dundar4',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [1.tercihiniz]': 'Eğitim',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [2.tercihiniz]': 'Kariyer',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [3.tercihiniz]': 'Kişisel Gelişim ve Sanat',
+    'Eğitim kategorisinde hangi atölyeleri seçmek istersiniz?': 'SQL',
+    'Kariyer kategorisinde hangi atölyeleri seçmek istersiniz?': 'Kariyer Yolu Belirleme',
+    'Kişisel Gelişim ve Sanat kategorisinde hangi atölyeleri seçmek istersiniz?': 'Hikayeleştime',
+    'Telefon Numaranız': '5397062143',
+    'Hangi okuldasınız?': 'Beykent Universitesi',
+    ' Bölümünüz-Sınıfınız ': 'Bilgisayar Muhendisligi - 4',
+    'Egitim kategorisinde hangi egitimleri secmek istersiniz?': 'SQL-Cengiz Atilla (Saat:12:00-17:30 )(2 gün)',
+    'Kariyer kategorisinde hangi egitimleri secmek istersiniz?':
+      'Mülakat Teknik Ve Simülasyonları LERZAN ERKAN ( Saat 14:00-17:30)',
+    'Kişisel Gelişim kategorisinde hangi egitimleri secmek istersiniz?':
+      'Hikayeleştirme-STORYTELLING (Saat 13:00-14:30)',
+    'Sanat kategorisinde hangi egitimleri secmek istersiniz?': 'C EĞİTİMİ - SAAT BİLGİSİ',
+    'Mail Adresiniz': 'ozn.dndr921@gmail.com',
+    'Öğrenci misiniz?': 'Evet'
+  },
+  {
+    'Zaman damgası': '2/24/2020 18:20:16',
+    'Adınız Soyadınız': 'Ozan Dundar5',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [1.tercihiniz]': 'Eğitim',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [2.tercihiniz]': 'Kariyer',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [3.tercihiniz]': 'Kişisel Gelişim ve Sanat',
+    'Eğitim kategorisinde hangi atölyeleri seçmek istersiniz?': 'SQL',
+    'Kariyer kategorisinde hangi atölyeleri seçmek istersiniz?': 'Kariyer Yolu Belirleme',
+    'Kişisel Gelişim ve Sanat kategorisinde hangi atölyeleri seçmek istersiniz?': 'Hikayeleştime',
+    'Telefon Numaranız': '5397062143',
+    'Hangi okuldasınız?': 'Beykent Universitesi',
+    ' Bölümünüz-Sınıfınız ': 'Bilgisayar Muhendisligi - 4',
+    'Egitim kategorisinde hangi egitimleri secmek istersiniz?': 'SQL-Cengiz Atilla (Saat:12:00-17:30 )(2 gün)',
+    'Kariyer kategorisinde hangi egitimleri secmek istersiniz?':
+      'Mülakat Teknik Ve Simülasyonları LERZAN ERKAN ( Saat 14:00-17:30)',
+    'Kişisel Gelişim kategorisinde hangi egitimleri secmek istersiniz?':
+      'Hikayeleştirme-STORYTELLING (Saat 13:00-14:30)',
+    'Sanat kategorisinde hangi egitimleri secmek istersiniz?': 'C EĞİTİMİ - SAAT BİLGİSİ',
+    'Mail Adresiniz': 'ozn.dndr921@gmail.com',
+    'Öğrenci misiniz?': 'Evet'
+  },
+  {
+    'Zaman damgası': '2/25/2020 1:35:57',
+    'Adınız Soyadınız': 'Onurcan Golge',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [1.tercihiniz]': 'Kişisel Gelişim ve Sanat',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [2.tercihiniz]': 'Kariyer',
+    'Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [3.tercihiniz]': 'Eğitim',
+    'Eğitim kategorisinde hangi atölyeleri seçmek istersiniz?': 'Javascript',
+    'Kariyer kategorisinde hangi atölyeleri seçmek istersiniz?': 'SEO Eğitimi, Web Eğitimi, Beyin Antrenörlüğü',
+    'Kişisel Gelişim ve Sanat kategorisinde hangi atölyeleri seçmek istersiniz?':
+      'Hikayeleştime, Gençler İçin Koçluk Eğitimi',
+    'Telefon Numaranız': '12345678',
+    'Hangi okuldasınız?': 'Prag University',
+    ' Bölümünüz-Sınıfınız ': 'Takiliyorum moruks - 2',
+    'Egitim kategorisinde hangi egitimleri secmek istersiniz? -2': 'JavaSCRİPT( Saat:11:00-17:30)(2 gün)',
+    'Kariyer kategorisinde hangi egitimleri secmek istersiniz? -2': 'Cv Hazırlama Teknikleri(Saat 11:00-13:30)',
+    'Kişisel Gelişim kategorisinde hangi egitimleri secmek istersiniz? -2': 'Hikayeleştirme',
+    'Sanat kategorisinde hangi egitimleri secmek istersiniz? -2': 'B EĞİTİMİ - SAAT BİLGİSİ',
+    'Mail Adresiniz': 'onurcangolge@gmail.com',
+    'Öğrenci misiniz?': 'Evet'
+  }
+];
+
+var lectures = [
+  {
+    name: 'SQL',
+    quota: 4,
+    duration: 5,
+    startsAt: 10.0
+  },
+  {
+    name: 'Javascript',
+    quota: 3,
+    duration: 10,
+    startsAt: 11.0
+  },
+  {
+    name: 'Java',
+    quota: 2,
+    duration: 3,
+    startsAt: 16.0
+  }
+];
+
+function areMatchable(first, second) {
+  var result = false;
+  var later, earlier;
+  if (first.startsAt < second.startsAt) {
+    earlier = first;
+    later = second;
+  } else if (first.startsAt > second.startsAt) {
+    earlier = first;
+    later = second;
+  } else {
+    // starts are equal. Should be choose one of them
+    console.log('starts are equal');
+    return result;
+  }
+
+  console.log({ later: later, earlier: earlier });
+
+  if (earlier.duration + earlier.startsAt <= later.startsAt) {
+    result = true;
+  }
+
+  return result;
+}
+
+function checkLectures(lecs, newLec) {
+  if (lecs.length === 0) {
+    return true;
+  } else {
+    var result = false;
+
+    // for (var i = 0; i < lecs.length; i++) {
+    //   for (var j = 0; j < lecs.length; j++) {
+    //     if (lecs[i].name !== lecs[j].name) {
+    //       result = areMatchable(lecs[i], lecs[j]);
+    //     }
+    //   }
+    // }
+
+    for(var i = 0; i < lecs.length; i++){
+      result = areMatchable(lecs[i], newLec);
+    } // 3 ders secmek suan icin mumkun degil.
+    // yarin test et
+
+    return result;
+  }
+}
+
+function selection(obj) {
+  var result = {
+    name: obj['Adınız Soyadınız'],
+    lectures: []
+  };
+  var categoryChoices = [
+    obj['Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [1.tercihiniz]'],
+    obj['Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [2.tercihiniz]'],
+    obj['Öncelikli olarak katılmak istediğiniz atölye kategorisi hangisidir? [3.tercihiniz]']
+  ];
+  var lectureChoices = [];
+
+  categoryChoices.forEach(function(i) {
+    var choosenLectures = obj[`${i} kategorisinde hangi atölyeleri seçmek istersiniz?`];
+    var splicedLectures;
+    if (choosenLectures.search(',') >= 0) {
+      splicedLectures = choosenLectures.split(', ');
+      var lecture = {
+        category: i,
+        name: splicedLectures
+      };
+    } else {
+      var lecture = {
+        category: i,
+        name: [ choosenLectures ]
+      };
+    }
+    lectureChoices.push(lecture);
+  });
+  // console.log(lectureChoices);
+
+  lectures.forEach(function(i) {
+    lectureChoices[0].name.forEach(function(j) {
+      if (i.name === j) {
+        if (i.quota > 0) {
+          if (checkLectures(result.lectures, i)) {
+            /* 
+            genel sonucumda eger kisiye ait ders yoksa,
+            i olan sectigi lectureyi, genel sonucumun icine koy(happens in function).
+            Eger kisiye ait ders varsa,
+            kisinin dersleri ile i degeri uyumlu mu kontrol et
+            */
+            result.lectures.push(i);
+            i.quota--;
+          }
+        }
+      }
+    });
+  });
+  console.log(result);
+}
+
+data.forEach(function(d) {
+  selection(d);
+});
